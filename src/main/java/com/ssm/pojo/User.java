ea6d822f1a923b9408029a65fbbfb55afce1ea2d
DYT <2021095837@qq.com>
2019-04-22 11:20:49 +0800

    de

---
+++ src/main/java/com/ssm/pojo/User.java
@@ -0,0 +1,24 @@
+package com.ssm.pojo;
+
+
+public class User {
+    public User() {
+
+    }
+    private int i;
+
+    public int getI() {
+        return i;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+}
